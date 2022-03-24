b107bd9f42e1364c6bab54b6c6095cb6598c9b5c
James Barry <jbm03003@gmail.com>
2022-03-24 15:51:56 -0400

    added offline functionality

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -59,7 +59,7 @@ const handlePizzaSubmit = event => {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(formData)
-  }) 
+  })
     .then(response => response.json())
     .then(postResponse => {
       alert('Pizza created successfully!');
